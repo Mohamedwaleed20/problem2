2a65a742d1fdbeded949877e6b7ddf95ba7cbfea
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-14 10:50:54 +0300

    Add project files.

---
+++ problem2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("please enter the num : ");
+double num = double.Parse(Console.ReadLine());
+if (num % 2 == 0 )
+{
+    Console.WriteLine("num is even ");
+}
+else
+{
+    Console.WriteLine("num is odd ");
+}
+Console.ReadKey();
